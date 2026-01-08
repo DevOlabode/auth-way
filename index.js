6169ed582e62fb53ffa72dd72e66c52cb93e1a17
Samuel Olabode <Solabode499@gmail.com>
2026-01-07 18:04:05 -0700

    Successfully set up EJS for the frontend.

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.use(express.json({ limit: '10mb' }));
+const ejsMate = require('ejs-mate');
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 require('./config/database')();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.engine('ejs', ejsMate);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT;
 app.listen(PORT, ()=>{
