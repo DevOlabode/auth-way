ae38a4ccdc0b039eb99d71fc13da9b3928312fc4
Samuel Olabode <Solabode499@gmail.com>
2026-01-07 18:00:31 -0700

    Connected local mongoDB successfully.

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+require('./config/database')();
 
 const PORT = process.env.PORT;
 app.listen(PORT, ()=>{
